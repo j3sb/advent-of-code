4fce536f2c67adac27259b199b0c7fd3470d72a3
jonasb <jonas.bonnaudet@mindroad.se>
2025-12-10 10:12:21 +0100

    day 8.2

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashSet, fs};
+use std::{arch::x86_64::CpuidResult, collections::HashSet, fs, vec};
 
 fn dist((x1, y1, z1): &(isize, isize, isize), (x2, y2, z2): &(isize, isize, isize)) -> isize {
     let dx = x1 - x2;
@@ -21,6 +21,23 @@ fn get_circuits(circuits: &mut Vec<HashSet<usize>>) {
             } else {
                 j += 1;
             }
+        }
+        i += 1;
+    }
+
+    if modified {
+        get_circuits(circuits);
+    }
+}
+
+fn add_circuit(circuits: &mut Vec<HashSet<usize>>, connection: &HashSet<usize>) {
+    let mut modified = false;
+    let mut i = 0;
+    while i < circuits.len() {
+        if connection.iter().any(|p| circuits[i].contains(p)) {
+            // put both together
+            circuits[i].extend(connection);
+            modified = true;
         }
         i += 1;
     }
@@ -57,25 +74,101 @@ fn main() {
 
     dists.sort_by_key(|&(d, _, _)| d);
 
-    // println!("{:?}", dists);
-
-    let mut connections: Vec<HashSet<usize>> = dists
+    let connections: Vec<HashSet<usize>> = dists
         .iter()
-        .take(num_pairs)
+        // .take(num_pairs)
         .map(|(_, i, j)| vec![*i, *j].into_iter().collect::<HashSet<usize>>())
         .collect();
 
-    println!("{:?}", connections);
-    // let connections_vec = connections.iter().map(|p| p.to_vec()).collect();
-    get_circuits(&mut connections);
+    // println!("{:?}", connections);
+    let mut circuits: Vec<HashSet<usize>> = vec![];
+    let mut last_connection: &HashSet<usize> = &HashSet::new();
+    for connection in &connections {
+        last_connection = &connection;
+        circuits.push(connection.clone());
+        get_circuits(&mut circuits);
+        if circuits.len() == 1 && circuits[0].len() == points.len() {
+            break;
+        }
+    }
 
-    println!("{:?}", connections);
-
-    let mut lengths: Vec<usize> = connections.iter().map(|c| c.len()).collect::<Vec<usize>>();
-    lengths.sort_by_key(|&l| std::cmp::Reverse(l));
-
-    println!("{:?}", lengths);
-
-    let res = lengths.iter().take(3).fold(1, |acc, l| acc * l);
-    println!("{res}");
+    let prod = last_connection
+        .iter()
+        .fold(1isize, |acc, i| acc * points[*i].0);
+    println!("{prod}");
 }
+
+// part 1
+
+// fn get_circuits(circuits: &mut Vec<HashSet<usize>>) {
+//     let mut modified = false;
+//     let mut i = 0;
+//     while i < circuits.len() {
+//         let mut j = i + 1;
+//         while j < circuits.len() {
+//             if circuits[i].iter().any(|p| circuits[j].contains(p)) {
+//                 // put both together
+//                 let to_merge = circuits.remove(j);
+//                 circuits[i].extend(to_merge);
+//                 modified = true;
+//             } else {
+//                 j += 1;
+//             }
+//         }
+//         i += 1;
+//     }
+
+//     if modified {
+//         get_circuits(circuits);
+//     }
+// }
+
+// fn main() {
+//     let data = fs::read_to_string("./input.txt").expect("an input file");
+//     let num_pairs = 1000;
+
+//     let points: Vec<(isize, isize, isize)> = data
+//         .split("\n")
+//         .map(|s| {
+//             let mut nums = s.split(",").map(|v| v.parse::<isize>().expect("a isize"));
+
+//             let a = nums.next().expect("missing first number");
+//             let b = nums.next().expect("missing second number");
+//             let c = nums.next().expect("missing third number");
+
+//             (a, b, c)
+//         })
+//         .collect();
+
+//     let mut dists: Vec<(isize, usize, usize)> = vec![];
+//     for (i, p1) in points.iter().enumerate() {
+//         for (j, p2) in points.iter().skip(i + 1).enumerate() {
+//             let d = dist(p1, p2);
+//             dists.push((d, i, j + i + 1));
+//         }
+//     }
+
+//     dists.sort_by_key(|&(d, _, _)| d);
+
+//     // println!("{:?}", dists);
+
+//     let mut connections: Vec<HashSet<usize>> = dists
+//         .iter()
+//         .take(num_pairs)
+//         .map(|(_, i, j)| vec![*i, *j].into_iter().collect::<HashSet<usize>>())
+//         .collect();
+
+//     println!("{:?}", connections);
+//     // let connections_vec = connections.iter().map(|p| p.to_vec()).collect();
+//     get_circuits(&mut connections);
+
+//     println!("{:?}", connections);
+
+//     let mut lengths: Vec<usize> = connections.iter().map(|c| c.len()).collect::<Vec<usize>>();
+//     lengths.sort_by_key(|&l| std::cmp::Reverse(l));
+
+//     println!("{:?}", lengths);
+
+//     let res = lengths.iter().take(3).fold(1, |acc, l| acc * l);
+//     println!("{res}");
+// }
